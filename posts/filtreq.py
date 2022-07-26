b5723543a803c02f39d5bb95d725d6dc505e479c
unknown <issa97403@gmail.com>
2022-07-26 02:44:13 +0200

    verion1

---
+++ posts/filtreq.py
@@ -0,0 +1,13 @@
+class PurchaseList(generics.ListAPIView):
+    serializer_class = PurchaseSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Purchase.objects.all()
+        username = self.request.query_params.get('username')
+        if username is not None:
+            queryset = queryset.filter(purchaser__username=username)
+        return queryset
